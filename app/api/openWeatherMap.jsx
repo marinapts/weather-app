02273e53fa162bf4b89809683c91b658dc354c12
Marina Potsi <potsimarina@gmail.com>
2017-01-03 22:56:32 +0000

    Added copy

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -3,19 +3,19 @@ var axios = require('axios');
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?&appid=9cfad684f0037d55afaabb029c7ee2d4&units=metric';
 
 module.exports = {
-	getTemp: function (city) {
-		var encodedCity = encodeURIComponent(city);
-		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedCity}`;
+  getTemp: function (city) {
+    var encodedCity = encodeURIComponent(city);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedCity}`;
 
-		return axios.get(requestUrl).then(function (res) {
-			if (res.data.cod && res.data.message) {
-				throw new Error(res.data.message);
-			}
-			else {
-				return res.data.main.temp;
-			}
-		}, function (res) {
-			throw new Error(res.data.message);
-		});
-	}
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      }
+      else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
 }
